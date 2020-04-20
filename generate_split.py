85dd2f2d9a549da330c2ec77d3fe585207a417f6
Di <di1113@me.com>
2020-04-20 00:42:43 -0700

    finished split functions

--- generate_split.py
+++ generate_split.py
@@ -1,12 +1,13 @@
 import numpy as np
 import os
+import json
 
 np.random.seed(2020) # to ensure you always get the same train/test split
 
-data_path = '../data/RedLights2011_Medium'
-gts_path = '../data/hw02_annotations'
-split_path = '../data/hw02_splits'
-os.makedirs(preds_path, exist_ok=True) # create directory if needed
+data_path = '../../data/RedLights2011_Medium'
+gts_path = '../../data/hw02_annotations'
+split_path = '../../data/hw02_splits'
+os.makedirs(split_path, exist_ok=True) # create directory if needed
 
 split_test = False # set to True and run when annotations are available
 
@@ -25,6 +26,13 @@ file_names_test = []
 Your code below. 
 '''
 
+np.random.shuffle(file_names)
+train_size = int(train_frac*len(file_names))
+file_names_train = file_names[:train_size]
+file_names_test = file_names[train_size :]
+# print(len(file_names_train))
+# print(len(file_names_test))
+
 assert (len(file_names_train) + len(file_names_test)) == len(file_names)
 assert len(np.intersect1d(file_names_train,file_names_test)) == 0
 
@@ -32,8 +40,9 @@ np.save(os.path.join(split_path,'file_names_train.npy'),file_names_train)
 np.save(os.path.join(split_path,'file_names_test.npy'),file_names_test)
 
 if split_test:
-    with open(os.path.join(gts_path, 'annotations.json'),'r') as f:
+    with open(os.path.join(gts_path, 'formatted_annotations_mturk.json'),'r') as f:
         gts = json.load(f)
+        # print(json.dumps(gts, sort_keys=True, indent=4))
     
     # Use file_names_train and file_names_test to apply the split to the
     # annotations
@@ -42,7 +51,19 @@ if split_test:
     '''
     Your code below. 
     '''
-    
+    # convert list to set for faster lookup 
+    test_set = set(file_names_test)
+    for key in gts:
+        if key in test_set:
+            gts_test[key] = gts.get(key)
+        else: 
+            gts_train[key] = gts.get(key)
+        
+    # print(json.dumps(gts_train, sort_keys=True, indent=4))
+    # print(json.dumps(gts_test, sort_keys=True, indent=4))
+    # print(len(gts_train))
+    # print(len(gts_test)) 
+
     with open(os.path.join(gts_path, 'annotations_train.json'),'w') as f:
         json.dump(gts_train,f)
     
